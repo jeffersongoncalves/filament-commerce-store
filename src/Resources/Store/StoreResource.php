74d3e86dd236aef90c9a0c619a5250d88f0b665f
jeffersongoncalves <gerson.simao.92@gmail.com>
2026-06-30 13:35:18 +0000

    1.x: Filament v3 branch

--- src/Resources/Store/StoreResource.php
+++ src/Resources/Store/StoreResource.php
@@ -2,8 +2,8 @@
 
 namespace JeffersonGoncalves\FilamentCommerce\Store\Resources\Store;
 
+use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use JeffersonGoncalves\Commerce\Store\Models\Store;
 use JeffersonGoncalves\FilamentCommerce\Store\CommerceStorePlugin;
@@ -26,9 +26,9 @@ class StoreResource extends Resource
         }
     }
 
-    public static function form(Schema $schema): Schema
+    public static function form(Form $form): Form
     {
-        return StoreForm::configure($schema);
+        return StoreForm::configure($form);
     }
 
     public static function table(Table $table): Table
