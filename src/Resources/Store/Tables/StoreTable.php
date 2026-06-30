d24a977fa8070177436e974aa3cf99ebc5946dc9
jeffersongoncalves <gerson.simao.92@gmail.com>
2026-06-30 19:11:07 +0000

    P3 backport to Filament v3 (1.x)

--- src/Resources/Store/Tables/StoreTable.php
+++ src/Resources/Store/Tables/StoreTable.php
@@ -2,7 +2,7 @@
 
 namespace JeffersonGoncalves\FilamentCommerce\Store\Resources\Store\Tables;
 
-use Filament\Actions;
+use Filament\Tables\Actions;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -18,11 +18,11 @@ class StoreTable
                 TextColumn::make('default_currency_code')
                     ->toggleable(),
             ])
-            ->recordActions([
+            ->actions([
                 Actions\EditAction::make(),
                 Actions\DeleteAction::make(),
             ])
-            ->toolbarActions([
+            ->bulkActions([
                 Actions\BulkActionGroup::make([
                     Actions\DeleteBulkAction::make(),
                 ]),
